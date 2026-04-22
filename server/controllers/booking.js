604baf326cc3f74adcbdea6ddd37a3736cb57c5c
Karina Koldaeva <karina.mila@yandex.ru>
2026-04-22 21:54:36 +0300

    feat: create restaurants server logic

---
+++ server/controllers/booking.js
@@ -0,0 +1,40 @@
+const Booking = require("../models/Booking");
+const ROLES = require("../constants/roles");
+
+async function getBusyTables(restaurantId, date, time) {
+  const bookings = await Booking.find({
+    restaurant: restaurantId,
+    date,
+    time,
+  });
+
+  return bookings.map((b) => b.tableNumber);
+}
+
+async function addBooking(bookingData) {
+  const newBooking = await Booking.create(bookingData);
+  return newBooking;
+}
+
+async function deleteBooking(bookingId, userId, userRole) {
+  const booking = await Booking.findById(bookingId);
+
+  if (!booking) {
+    throw new Error("Бронирование не найдено");
+  }
+
+  const isPrivilegedUser = [ROLES.ADMIN, ROLES.MODERATOR].includes(userRole);
+  const isOwner = booking.user.toString() === userId.toString();
+
+  if (!isOwner && !isPrivilegedUser) {
+    throw new Error("У вас нет прав на отмену этого бронирования");
+  }
+
+  await Booking.deleteOne({ _id: bookingId });
+}
+
+module.exports = {
+  getBusyTables,
+  addBooking,
+  deleteBooking,
+};
