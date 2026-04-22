7a458a1a4b7891aca2c1b59f0cbb983b13889d62
Karina Koldaeva <karina.mila@yandex.ru>
2026-04-22 22:05:14 +0300

    feat: create Auth&Register

---
+++ client/src/pages/RegPage/RegPage.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate } from 'react-router';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Input } from '../../components';
+import { setUser } from '../../actions';
+import { selectUserRole } from '../../selectors';
+import { ROLE } from '../../constants';
+import { useResetForm } from '../../hooks';
+import styled from 'styled-components';
+import { request } from '../../utils/request';
+import { Button } from '../../components/buttons/Button';
+
+const regFormSchema = yup.object().shape({
+	login: yup
+		.string()
+		.required('Заполните логин')
+		.matches(/^\w+$/, 'Неверно заполнен логин. Допускаются только буквы и цифры')
+		.min(3, 'Неверно заполнен. Минимум 3 символов')
+		.max(15, 'Неверно заполнен. Максимум 15 символов'),
+	password: yup
+		.string()
+		.required('Заполните пароль')
+		.matches(
+			/^[\w#%]+$/,
+			'Неверно заполнен пароль. Допускаются буквы, цифры и знаки № %',
+		)
+		.min(6, 'Неверно заполнен пароль. Минимум 6 символов')
+		.max(30, 'Неверно заполнен пароль. Максимум 30 символов'),
+	passcheck: yup
+		.string()
+		.required('Заполните повтор пароля')
+		.oneOf([yup.ref('password'), null], 'Повтор пароля не совпадает'),
+});
+
+const RegistrationContainer = ({ className }) => {
+	const {
+		register,
+		reset,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			login: '',
+			password: '',
+			passcheck: '',
+		},
+		resolver: yupResolver(regFormSchema),
+	});
+
+	const [serverError, setServerError] = useState(null);
+	const dispatch = useDispatch();
+	const roleId = useSelector(selectUserRole);
+	useResetForm(reset);
+
+	const onSubmit = ({ login, password }) => {
+		request('/register', 'POST', { login, password }).then(({ error, user }) => {
+			if (error) {
+				setServerError(`Ошибка запроса: ${error}`);
+				return;
+			}
+
+			dispatch(setUser(user));
+			sessionStorage.setItem('userData', JSON.stringify(user));
+		});
+	};
+
+	const formError =
+		errors?.login?.message || errors?.password?.message || errors?.passcheck?.message;
+	const errorMessage = formError || serverError;
+
+	if (roleId !== ROLE.GUEST) {
+		return <Navigate to="/" />;
+	}
+
+	return (
+		<div className={className}>
+			<h2>Регистрация</h2>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Input
+					type="text"
+					name="login"
+					placeholder="Логин..."
+					{...register('login', {
+						onChange: () => setServerError(null),
+					})}
+				/>
+				<Input
+					type="password"
+					name="password"
+					placeholder="Пароль..."
+					{...register('password', {
+						onChange: () => setServerError(null),
+					})}
+				/>
+				<Input
+					type="password"
+					name="passcheck"
+					placeholder="Проверка пароля..."
+					{...register('passcheck', {
+						onChange: () => setServerError(null),
+					})}
+				/>
+				<Button type="submit" disabled={!!formError}>
+					Зарегистрироваться
+				</Button>
+				{errorMessage && <AuthFormError>{errorMessage}</AuthFormError>}
+			</form>
+		</div>
+	);
+};
+
+export const RegPage = styled(RegistrationContainer)`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	& > form {
+		display: flex;
+		flex-direction: column;
+		width: 260px;
+	}
+`;
+
+export const AuthFormError = styled.div`
+	background-color: #fcadad;
+	padding: 10px;
+	margin: 10px 0 0;
+	font-size: 18px;
+`;
