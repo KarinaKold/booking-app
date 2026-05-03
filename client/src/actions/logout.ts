801039bfa65df742ab2e9395e92099723da76898
Karina Koldaeva <karina.mila@yandex.ru>
2026-05-03 23:33:08 +0300

    fix: async logout

--- client/src/actions/logout.ts
+++ client/src/actions/logout.ts
@@ -1,10 +1,12 @@
+import type { Dispatch } from 'redux';
 import { request } from '../utils/request';
 import { ACTION_TYPE } from './action-type';
 
-export const logout = () => {
-	request('/logout', 'POST');
-
-	return {
-		type: ACTION_TYPE.LOGOUT,
-	};
+export const logout = () => async (dispatch: Dispatch) => {
+	try {
+		await request('/logout', 'POST');
+		dispatch({ type: ACTION_TYPE.LOGOUT });
+	} catch (error) {
+		console.error('Ошибка при выходе:', error);
+	}
 };
